59ca5e0619b4b7f2e4dec54d19b1b247fbb58930
Jacco12 <molsjacco@gmail.com>
2026-05-20 16:00:01 +0200

    fetched pictures of fish

---
+++ js/picture-of-month.js
@@ -0,0 +1,17 @@
+async function init() {
+    await laadCSV();
+
+    const snapshot = geformatteerdeData
+    .map(item =>item.snapshot_url)
+    .filter(Boolean)
+    .slice(0, 10)
+    console.log("SnapSHOTS:", snapshot);
+
+    const snapshotList = document.getElementById('fish-piture-list');
+
+    snapshotList.innerHTML = snapshot
+        .filter(url => url)
+        .map(url => `<li><img src=${url} alt="snapshot of fish"></li>`)
+        .join("");
+}
+init();
