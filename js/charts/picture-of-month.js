622644cbb9739772f16e8973c4944218586294e2
Jacco12 <molsjacco@gmail.com>
2026-05-21 11:35:23 +0200

    added styling for picture grid - and fish filter

--- js/charts/picture-of-month.js
+++ js/charts/picture-of-month.js
@@ -3,18 +3,21 @@ import { loadCsvData } from "../data/data-fetch.js"
 export async function fetchSnapshot() {
     const data = await loadCsvData("assets/data/website_event-week.csv");
 
-    console.log("pics", data)
     const snapshot = data
     .map(item =>item.snapshot_url)
     .filter(Boolean)
-    .slice(0, 10)
-    console.log("SnapSHOTS:", snapshot);
+    .slice(0, 40)
+    console.log("SNAPSHOTS:", snapshot);
+
+    const fishSnapshots = data
+    .filter(item => item.snapshot_url && item.fish_name && item.fish_name !== "Unknown" && item.fish_name !== "Geen vis-event")
+    .slice(0, 40);
+    console.log(fishSnapshots)
 
     const snapshotList = document.getElementById('fish-piture-list');
 
-    snapshotList.innerHTML = snapshot
-        .filter(url => url)
-        .map(url => `<li><img src="${url}" alt="snapshot of fish"></li>`)
+    snapshotList.innerHTML = fishSnapshots
+        .map(item => `<li class="snapshot-li"><img class="snapshot-img" src="${item.snapshot_url}" alt="snapshot-of-${item.snapshot_name}"></li>`)
         .join("");
 }
 fetchSnapshot();
