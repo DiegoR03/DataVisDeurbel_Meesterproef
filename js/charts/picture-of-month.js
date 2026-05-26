a4c38dc0274572a972608c7ac0764a6080689e20
Jacco12 <molsjacco@gmail.com>
2026-05-26 15:28:06 +0200

    start guess the fish and added fish pngs

--- js/charts/picture-of-month.js
+++ js/charts/picture-of-month.js
@@ -17,68 +17,39 @@ export async function fetchSnapshot() {
                     item.fish_name !== "onbekend" &&
                     item.fish_name !== "Geen vis-event")
     .slice(0, 40);
-    console.log(fishSnapshots)
 
-    const snapshotList = document.getElementById('fish-piture-list');
+    // const snapshotList = document.getElementById('fish-picture-list');
 
-    snapshotList.innerHTML = fishSnapshots
-        .map(item =>
-            `<li class="snapshot-li">
-                <figure>
-                    <img    class="snapshot-img"
-                            src="${item.snapshot_url}"
-                            alt="snapshot-of-${item.fish_name}"
-                            data-fish="${item.fish_name}"
-                            data-date="${item.created_at}">
-                </figure>
-            </li>
-        `)
-        .join("");
+    // snapshotList.innerHTML = fishSnapshots
+    //     .map(item =>
+    //         `<li class="snapshot-li">
+    //             <img    class="snapshot-img"
+    //                     src="${item.snapshot_url}"
+    //                     alt="snapshot-of-${item.fish_name}"
+    //                     data-fish="${item.fish_name}"
+    //                     data-date="${item.created_at}">
+    //         </li>
+    //     `)
+    //     .join("");
 
-
-    const fishTip = document.getElementById("fish-tip");
 
     const fishPopup = document.querySelector('.fish-picture-popup');
-    const fishPopupImg = document.querySelector('.fish-popup-img');
-    const fishPopupHeader = document.querySelector('.fish-popup-name');
-    const fishPopupButton = document.querySelector('.fish-popup-button')
+    const fishPopupButton = document.querySelector('.fish-popup-button');
 
-    // Showcase the name of the fish on hover 
-    document.querySelectorAll('.snapshot-img').forEach(img => {
-        img.addEventListener("mouseenter", (event) => {
-            const fishName = img.dataset.fish;
+    const fishImg = document.getElementById('fish-image');
 
-            fishTip.style.display = "block";
-            fishTip.innerHTML =
-            `<strong>${fishName}</strong>`
-        })
+    let currentFish = null
 
-        img.addEventListener("mousemove", (event) => {
-            fishTip.style.left = `${event.pageX + 15}px`
-            fishTip.style.top = `${event.pageY - 40}px`
-        })
+    function getRandomFish() {
+        const randomFish = Math.floor(Math.random() * fishSnapshots.length);
 
-        img.addEventListener("mouseleave", (event) => {
-            fishTip.style.display = "none"
-        })
+        currentFish = fishSnapshots[randomFish];
 
-        img.addEventListener('click', (event) => {
-            const fishTitle = img.dataset.fish;
-            const snapshotDate = img.dataset.date;
+        fishImg.src = currentFish.snapshot_url;
+        fishImg.alt = currentFish.alt
 
-            fishPopup.style.display = "flex";
-            fishPopupButton.style.display = "flex";
-            fishPopup.classList.add('fish-picture-slide-popup')
+    }
 
-            fishPopupImg.src = img.src;
-            fishPopupImg.alt = img.alt;
-
-            fishPopupHeader.innerHTML = 
-            `
-            <h3>${fishTitle} - ${snapshotDate}</h3>
-            `;
-        })
-    })
 
     fishPopupButton.addEventListener('click', () => {
         fishPopup.style.display = "none";
